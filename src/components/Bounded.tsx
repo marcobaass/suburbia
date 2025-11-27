95415ae1783efce10c9992a0ec637c0262b32e17
Marco Baass <picture@marcobaass.com>
2025-11-27 11:15:51 +0100

    Fix: Type Error

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties, ElementType, ReactNode } from "react";
+import { CSSProperties, ElementType, ReactNode, createElement } from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
@@ -6,6 +6,7 @@ type BoundedProps = {
   className?: string;
   style?: CSSProperties;
   children: ReactNode;
+  [key: string]: any;
 };
 
 export function Bounded({
@@ -14,15 +15,15 @@ export function Bounded({
   children,
   ...restProps
 }: BoundedProps) {
-  return (
-    <Comp
-      className={clsx(
+  return createElement(
+    Comp,
+    {
+      className: clsx(
         "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-        className,
-      )}
-      {...restProps}
-    >
-      <div className="mx-auto w-full max-w-6xl">{children}</div>
-    </Comp>
+        className
+      ),
+      ...restProps,
+    },
+    <div className="mx-auto w-full max-w-6xl">{children}</div>
   );
 }
